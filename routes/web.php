67d6ff9dbcf43dd684d3dab5127af2e896a75a4e
John Arthur <john@jtarthur.net>
2017-09-30 09:04:11 -0500

    Add the views to the project, along with (compiled) assets.

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+
+Route::view('about', 'about')->name('about');
+
+Route::view('experience', 'experience')->name('experience');
+
+Route::view('contact', 'contact')->name('contact');
